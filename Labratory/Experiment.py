8f6e4fe2f1ec61dd07325723a0c1133907a51135
subham <sshoobham10@gmail.com>
2021-02-23 11:18:10 +0545

    Initial commit

---
+++ Labratory/Experiment.py
@@ -0,0 +1,8 @@
+#Write a program that takes three numbers and prints their sum. Every number is going on a seperate line.
+
+a=int(input('enter the number'))
+b=int(input('enter the number'))
+c=int(input('enter the number'))
+
+sum= a + b + c
+print(f' The sum is {sum}')
