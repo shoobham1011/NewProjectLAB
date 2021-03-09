6eaf1cfa4cf9e357c35615160251df326eebe612
subham <sshoobham10@gmail.com>
2021-03-09 12:03:32 +0545

    initial commit

---
+++ Labratory/Experiment9.py
@@ -0,0 +1,11 @@
+'''
+write the python program which accepts which accepts the radious of a circle from the user and complete the area.
+area of circle = pi * r**2
+'''
+
+radius = int(input('enter the radius : '))
+pi = 3.14
+
+area_of_circle = pi * (radius**2)
+
+print(f'The area of cirlce is {area_of_circle}.')
