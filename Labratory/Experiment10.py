61711adeaca69766d81b9428486cdb55ba47b250
subham <sshoobham10@gmail.com>
2021-03-09 12:09:39 +0545

    initial commit

---
+++ Labratory/Experiment10.py
@@ -0,0 +1,10 @@
+'''
+write the python program to find sum of first n positive number
+sum = (n*(n+1))/2
+'''
+
+n = float(input('enter the n number : '))
+
+sum = (n*(n+1))/2
+
+print(f'the sum of first n positive number is {sum}')
